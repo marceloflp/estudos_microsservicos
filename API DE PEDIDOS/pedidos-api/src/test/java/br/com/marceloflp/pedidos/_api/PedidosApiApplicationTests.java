0681343718ccbfbc1fde913d440538f86d75dc8e
marceloflp <mfelipedasilva2020@gmail.com>
2026-03-20 15:22:50 -0300

    Iniciando criacao de API de pedidos

---
+++ API DE PEDIDOS/pedidos-api/src/test/java/br/com/marceloflp/pedidos/_api/PedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marceloflp.pedidos._api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
