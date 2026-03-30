c0ecddd49cfb36431c1f07a3549d5b5a78beed55
marceloflp <mfelipedasilva2020@gmail.com>
2026-03-30 13:51:43 -0300

    Criando classe PedidoService

---
+++ API DE PEDIDOS/pedidos-api/src/main/java/br/com/marceloflp/pedidos/_api/service/PedidoService.java
@@ -0,0 +1,29 @@
+package br.com.marceloflp.pedidos._api.service;
+import br.com.marceloflp.pedidos._api.entities.Pedido;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    private final Logger logger = LoggerFactory.getLogger(PedidoService.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public PedidoService(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public Pedido enfileirarPedido(Pedido pedido){
+        rabbitTemplate.convertAndSend(exchangeName, "", pedido);
+        logger.info("Pedido enfileirado: {}", pedido.toString());
+        return pedido;
+    }
+}
